639d2338a9479d7fb47fc5994f2743e5cd3b1d8f
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-03-22 16:29:55 +0400

    Added sysbench database creation function for issue #1

--- tbtests/run_sysbench.py
+++ tbtests/run_sysbench.py
@@ -1,4 +1,4 @@
-from subprocess import Popen
+from subprocess import Popen, getstatusoutput
 from readconfig import config_reader
 
 
@@ -8,6 +8,41 @@ class SysbenchRun(config_reader.ConfigReader):
         self.conf = config
         super().__init__(self.conf)
 
+    def create_sysbench_db(self):
+        backup_command_connection = '{} -u{} --password={} --host={}'
+        execute = " -e 'create database {}'"
+        if hasattr(self, 'sysbench_db'):
+            new_command = backup_command_connection.format(
+                            self.mysql,
+                            self.mysql_user,
+                            self.mysql_password,
+                            self.mysql_host) + execute.format(self.sysbench_db)
+            status, output = getstatusoutput(new_command)
+
+            if status == 0:
+                print("Database Created!")
+                return True
+            else:
+                print("Failed to create specified database!")
+                return False
+        else:
+            new_command = backup_command_connection.format(
+                self.mysql,
+                self.mysql_user,
+                self.mysql_password,
+                self.mysql_host) + execute.format('sbtest')
+            status, output = getstatusoutput(new_command)
+
+            if status == 0:
+                print("Database Created!")
+                return True
+            else:
+                print("Failed to create specified database!")
+                return False
+
+
+
+
 
     def create_sysbench_command(self, sysbench_action):
         command = "sysbench  %s" \
@@ -15,15 +50,16 @@ class SysbenchRun(config_reader.ConfigReader):
                           " --mysql-user=%s --mysql-password=%s --db-driver=mysql " \
                           " --max-requests=0  --mysql-socket=%s %s"
         general_command = (command) % (self.sysbench_options, self.sysbench_db,
-                                       self.mysql_user, self.mysql_user,
+                                       self.mysql_user, self.mysql_password,
                                        self.mysql_socket, sysbench_action)
         return general_command
 
     def run_sysbench(self, command_to_run):
 
-        process = Popen(
-            command_to_run,
-            stdin=None,
-            stdout=None,
-            stderr=None)
+        if self.create_sysbench_db():
+            process = Popen(
+                command_to_run,
+                stdin=None,
+                stdout=None,
+                stderr=None)
 
