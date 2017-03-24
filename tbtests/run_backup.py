b43f79e78c302254ad574c1e5752e798b4b91647
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-03-24 11:32:43 +0400

    Trying to implement mutex between threads for issue #7

--- tbtests/run_backup.py
+++ tbtests/run_backup.py
@@ -2,12 +2,13 @@ from backup.backup_calculation import CheckMySQLEnvironment
 from os import makedirs
 from os.path import join
 from datetime import datetime
-
+from threading import Lock
 
 class BackupRun(CheckMySQLEnvironment):
     def __init__(self, config):
         self.conf = config
         super().__init__(self.conf)
+        self.mutex = Lock()
 
     def create_backup_directory(self):
         """
@@ -18,12 +19,14 @@ class BackupRun(CheckMySQLEnvironment):
                               datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
         try:
             # Creating backup directory
-            makedirs(new_backup_dir, exist_ok=True)
+            makedirs(new_backup_dir)
             return new_backup_dir
         except Exception as err:
             print("Something went wrong in create_backup_directory(): {}".format(err))
 
 
     def run_all(self):
+        self.mutex.acquire(1)
         backupdir = self.create_backup_directory()
         self.run_backup(backup_dir=backupdir)
+        self.mutex.release()
