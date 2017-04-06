f4877b4a0fc30d4bc315cc9132cf2cefac6b418f
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2017-04-06 12:25:13 +0400

    Bugfix issue7 (#10)
    * Preliminary fix for issue #7
    
    * Testing os.makedirs(exist_ok=True) for issue #7
    
    * Trying to implement mutex between threads for issue #7
    
    * Adding sleep(1) between folder creation and backup run for issue #7
    
    * Removing mutex for issue #7
    
    * Changed backup directory creation as per thread for issue #7
    
    * Running threads by tb_thread option from config file for issue #7

--- tbtests/run_backup.py
+++ tbtests/run_backup.py
@@ -1,21 +1,21 @@
 from backup.backup_calculation import CheckMySQLEnvironment
 from os import makedirs
 from os.path import join
-from datetime import datetime
-
 
 class BackupRun(CheckMySQLEnvironment):
     def __init__(self, config):
         self.conf = config
         super().__init__(self.conf)
 
-    def create_backup_directory(self):
+
+    def create_backup_directory(self, backup_dir):
         """
                 Creating timestamped backup directory.
                 :return: Newly created backup directory or Error.
                 """
-        new_backup_dir = join(self.backupdir,
-                              datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+        # new_backup_dir = join(self.backupdir,
+        #                       datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+        new_backup_dir = join(self.backupdir, backup_dir)
         try:
             # Creating backup directory
             makedirs(new_backup_dir)
@@ -24,6 +24,6 @@ class BackupRun(CheckMySQLEnvironment):
             print("Something went wrong in create_backup_directory(): {}".format(err))
 
 
-    def run_all(self):
-        backupdir = self.create_backup_directory()
+    def run_all(self, backup_dir):
+        backupdir = self.create_backup_directory(backup_dir=backup_dir)
         self.run_backup(backup_dir=backupdir)
