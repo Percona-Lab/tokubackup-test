530e96c9c987e33a9bcf47de8ee41aad955612eb
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-04-07 13:02:01 +0400

    Added slave credendtioals to backup command function for issue #12

--- tbtests/run_backup.py
+++ tbtests/run_backup.py
@@ -112,7 +112,7 @@ class BackupRun(CheckMySQLEnvironment):
             print("Something went wrong in run_backup(): {}".format(err))
 
 
-    def run_backup_with_output(self, backup_dir):
+    def run_backup_with_output(self, backup_dir, server_v = 0):
         backupdir = self.create_backup_directory(backup_dir=backup_dir)
         """
             Running actual backup command to MySQL server.
@@ -123,38 +123,70 @@ class BackupRun(CheckMySQLEnvironment):
         backup_command_execute = ' -e "set tokudb_backup_dir=\'{}\'"'
 
         try:
+            if server_v != 1:
+                if hasattr(self, 'mysql_socket'):
+                    backup_command_connection += ' --socket={}'
+                    backup_command_connection += backup_command_execute
+                    new_backup_command = backup_command_connection.format(
+                            self.mysql,
+                            self.mysql_user,
+                            self.mysql_password,
+                            self.mysql_host,
+                            self.mysql_socket,
+                            backupdir)
+                else:
+                    backup_command_connection += ' --port={}'
+                    backup_command_connection += backup_command_execute
+                    new_backup_command = backup_command_connection.format(
+                            self.mysql,
+                            self.mysql_user,
+                            self.mysql_password,
+                            self.mysql_host,
+                            self.mysql_port,
+                            backupdir)
+                # Do not return anything from subprocess
+                print(
+                    "Running backup command => %s" %
+                    (''.join(new_backup_command)))
 
-            if hasattr(self, 'mysql_socket'):
-                backup_command_connection += ' --socket={}'
-                backup_command_connection += backup_command_execute
-                new_backup_command = backup_command_connection.format(
-                        self.mysql,
-                        self.mysql_user,
-                        self.mysql_password,
-                        self.mysql_host,
-                        self.mysql_socket,
-                        backupdir)
+                status, output = subprocess.getstatusoutput(new_backup_command)
+                if status == 0:
+                    print("Backup completed!")
+                else:
+                    print("Backup failed!")
+                    print(output)
             else:
-                backup_command_connection += ' --port={}'
-                backup_command_connection += backup_command_execute
-                new_backup_command = backup_command_connection.format(
-                        self.mysql,
-                        self.mysql_user,
-                        self.mysql_password,
-                        self.mysql_host,
-                        self.mysql_port,
+                if hasattr(self, 'mysql_socket'):
+                    backup_command_connection += ' --socket={}'
+                    backup_command_connection += backup_command_execute
+                    new_backup_command = backup_command_connection.format(
+                        self.mysql_slv,
+                        self.user_slv,
+                        self.password_slv,
+                        self.host_slv,
+                        self.socket_slv,
                         backupdir)
-            # Do not return anything from subprocess
-            print(
-                "Running backup command => %s" %
-                (''.join(new_backup_command)))
+                else:
+                    backup_command_connection += ' --port={}'
+                    backup_command_connection += backup_command_execute
+                    new_backup_command = backup_command_connection.format(
+                        self.mysql_slv,
+                        self.user_slv,
+                        self.password_slv,
+                        self.host_slv,
+                        self.port_slv,
+                        backupdir)
+                # Do not return anything from subprocess
+                print(
+                    "Running backup command => %s" %
+                    (''.join(new_backup_command)))
 
-            status, output = subprocess.getstatusoutput(new_backup_command)
-            if status == 0:
-                print("Backup completed!")
-            else:
-                print("Backup failed!")
-                print(output)
+                status, output = subprocess.getstatusoutput(new_backup_command)
+                if status == 0:
+                    print("Backup completed!")
+                else:
+                    print("Backup failed!")
+                    print(output)
 
         except Exception as err:
             print("Something went wrong in run_backup(): {}".format(err))
